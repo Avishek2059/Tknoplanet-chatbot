61c522f0a427d14fc874254973ea08774da6b6ba
Avishek2059 <jaiswalavishek2002@gmail.com>
2024-10-21 11:33:10 +0545

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Tkno planet Chatbot',
+    version= '0.0.0',
+    author= 'Avishek Chaudhary',
+    author_email= 'jaiswalavishek2059@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
